6afd0bf19b8ac5e52304397b5da671e73437a7b0
root <root@ip-172-31-44-98.eu-north-1.compute.internal>
2026-04-07 19:41:50 +0000

    Compute module provisioning virtual machines and instance configurations

--- terraform/modules/compute/main.tf
+++ terraform/modules/compute/main.tf
@@ -1,42 +1,35 @@
-provider "aws" {
-  region = "us-west-2"
-}
-resource "aws_security_group" "dlr-sg" {
-  vpc_id = var.vpc_id
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  tags = {
-    Name = "dlr-sg"
-  }
-}
-resource "aws_instance" "dlr-ec2" {
-  ami           = "ami-0c94855ba95c71c99"
-  instance_type = "t2.micro"
-  subnet_id = var.public_subnet_ids[0]
-  vpc_security_group_ids = [aws_security_group.dlr-sg.id]
-  tags = {
-    Name = "dlr-ec2"
-  }
-}
-variable "vpc_id" {
+variable "resource_group_name" {
   type = string
 }
-variable "public_subnet_ids" {
-  type = list(string)
+variable "network_interface_id" {
+  type = string
 }
-variable "private_subnet_ids" {
-  type = list(string)
+variable "admin_username" {
+  type    = string
+  default = "adminuser"
 }
-output "security_group_id" {
-  value = aws_security_group.dlr-sg.id
+variable "admin_password" {
+  type    = string
+  default = "P@ssw0rd1234!"
+}
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "mylinuxvirtualmachine"
+  resource_group_name = var.resource_group_name
+  location            = "West US"
+  size                = "Standard_DS2_v2"
+  admin_username      = var.admin_username
+  admin_password      = var.admin_password
+  network_interface_ids = [
+    var.network_interface_id
+  ]
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
 }
