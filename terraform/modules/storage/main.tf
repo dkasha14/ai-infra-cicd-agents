0ad891128bc9489ce098ebc243e7a139001230ff
root <root@ip-172-31-44-98.eu-north-1.compute.internal>
2026-04-07 19:53:48 +0000

    Storage module configuring object storage resources and access policies

--- terraform/modules/storage/main.tf
+++ terraform/modules/storage/main.tf
@@ -1,1 +1,13 @@
-# No resources defined in this module for this requirement
+variable "bucket_name" {
+  default = "asha-bucket"
+}
+resource "aws_s3_bucket" "asha_bucket" {
+  bucket = var.bucket_name
+  tags = {
+    Project = "iac-agents"
+    Environment = "dev"
+  }
+}
+output "bucket_name" {
+  value = aws_s3_bucket.asha_bucket.id
+}
