8f8cf258f9d4aa1faa37838c7d6e29f45b308a68
root <root@ip-172-31-44-98.eu-north-1.compute.internal>
2026-04-07 19:41:38 +0000

    Network module defining VPC, subnets, and networking components for infrastructure setup

--- terraform/modules/network/main.tf
+++ terraform/modules/network/main.tf
@@ -1,74 +1,40 @@
-provider "aws" {
-  region = "us-west-2"
+variable "location" {
+  type    = string
+  default = "West US"
 }
-resource "aws_vpc" "dlr-vpc" {
-  cidr_block = "10.0.0.0/16"
-  tags = {
-    Name = "dlr-vpc"
+variable "resource_group_name" {
+  type    = string
+  default = "myresourcegroup"
+}
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+resource "azurerm_virtual_network" "vn" {
+  name                = "myvirtualnetwork"
+  address_space       = ["10.0.0.0/16"]
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+resource "azurerm_subnet" "sn" {
+  name                 = "mysubnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vn.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+resource "azurerm_network_interface" "ni" {
+  name                = "mynetworkinterface"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  ip_configuration {
+    name                          = "myipconfiguration"
+    subnet_id                     = azurerm_subnet.sn.id
+    private_ip_address_allocation = "Dynamic"
   }
 }
-resource "aws_internet_gateway" "dlr-igw" {
-  vpc_id = aws_vpc.dlr-vpc.id
-  tags = {
-    Name = "dlr-igw"
-  }
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
 }
-resource "aws_subnet" "dlr-public-subnet-1" {
-  vpc_id            = aws_vpc.dlr-vpc.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-west-2a"
-  tags = {
-    Name = "dlr-public-subnet-1"
-  }
-}
-resource "aws_subnet" "dlr-public-subnet-2" {
-  vpc_id            = aws_vpc.dlr-vpc.id
-  cidr_block        = "10.0.2.0/24"
-  availability_zone = "us-west-2b"
-  tags = {
-    Name = "dlr-public-subnet-2"
-  }
-}
-resource "aws_subnet" "dlr-private-subnet-1" {
-  vpc_id            = aws_vpc.dlr-vpc.id
-  cidr_block        = "10.0.3.0/24"
-  availability_zone = "us-west-2a"
-  tags = {
-    Name = "dlr-private-subnet-1"
-  }
-}
-resource "aws_subnet" "dlr-private-subnet-2" {
-  vpc_id            = aws_vpc.dlr-vpc.id
-  cidr_block        = "10.0.4.0/24"
-  availability_zone = "us-west-2b"
-  tags = {
-    Name = "dlr-private-subnet-2"
-  }
-}
-resource "aws_route_table" "dlr-public-rt" {
-  vpc_id = aws_vpc.dlr-vpc.id
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.dlr-igw.id
-  }
-  tags = {
-    Name = "dlr-public-rt"
-  }
-}
-resource "aws_route_table_association" "dlr-public-subnet-1" {
-  subnet_id      = aws_subnet.dlr-public-subnet-1.id
-  route_table_id = aws_route_table.dlr-public-rt.id
-}
-resource "aws_route_table_association" "dlr-public-subnet-2" {
-  subnet_id      = aws_subnet.dlr-public-subnet-2.id
-  route_table_id = aws_route_table.dlr-public-rt.id
-}
-output "vpc_id" {
-  value = aws_vpc.dlr-vpc.id
-}
-output "public_subnet_ids" {
-  value = [aws_subnet.dlr-public-subnet-1.id, aws_subnet.dlr-public-subnet-2.id]
-}
-output "private_subnet_ids" {
-  value = [aws_subnet.dlr-private-subnet-1.id, aws_subnet.dlr-private-subnet-2.id]
+output "network_interface_id" {
+  value = azurerm_network_interface.ni.id
 }
